6146d0014094c0cf9fa7a967800af8123992592e
党昊天 <dht925nerd@126.com>
2019-03-20 23:20:26 +0800

    update TreeNode

--- src/com/dang/binarytree/TreeNode.java
+++ src/com/dang/binarytree/TreeNode.java
@@ -10,9 +10,28 @@ public class TreeNode {
     public Integer val;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode parent;
 
     public TreeNode(Integer val) {
         this.val = val;
+    }
+
+    public TreeNode(Integer val, TreeNode parent){
+        this.val = val;
+        this.parent = parent;
+    }
+
+    /**
+     * 中序遍历前继
+     * @return 前继节点
+     */
+    public TreeNode succ(){
+        if (this.right == null) return this;
+        TreeNode temp = this.right.left;
+        while (temp.left != null){
+            temp = temp.left;
+        }
+        return temp;
     }
 
 }
