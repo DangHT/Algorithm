cde5d0b8472cd918474afe09b974cc282a28187a
Dang <dht925nerd@126.com>
2019-07-28 16:28:37 +0800

    2的幂

---
+++ src/com/dang/leetcode/Q_231.java
@@ -0,0 +1,40 @@
+package com.dang.leetcode;
+
+/**
+ * 2的幂 (https://leetcode-cn.com/problems/power-of-two/)
+ * @author dht
+ * @date 27/07/2019
+ */
+public class Q_231 {
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(218));
+    }
+
+    /**
+     * 我的答案，迭代验证法
+     * @param n
+     * @return
+     */
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        while (n > 0) {
+            if (n == 1) return true;
+            if (n % 2 == 1) return false;
+            n /= 2;
+        }
+        return true;
+    }
+
+    /**
+     * 位运算法
+     * @param n
+     * @return
+     */
+    public static boolean isPowerOfTwo_bit(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+}
